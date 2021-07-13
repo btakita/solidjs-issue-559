e3a5026c834819bfc7caae4ae68a34e3ac727f4f
Brian Takita <brian.takita@gmail.com>
2021-07-13 17:56:08 -0400

    + "jsxImportSource": "solid-js"

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,2 @@
 export declare const RouterContext: import("solid-js").Context<boolean | undefined>;
-export declare function foo(): any;
+export declare function foo(): import("solid-js").JSX.Element;
